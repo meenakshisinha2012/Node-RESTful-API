72b49d3b9ab745f9acf6588c31156e65ceeb626d
Maitraiya Mali <maitraiya1997@gmail.com>
2020-03-18 00:05:34 +0530

    Added next(); at the end of Cors code for passing control to next middleware.

--- app.js
+++ app.js
@@ -32,6 +32,7 @@ app.use((req,res,next) =>{
         res.header('Access-Control-Allow-Methods','PUT , POST , PATCH , DELETE , GET');
         return res.status(200).json({});
     }
+    next();
 });
 
 
